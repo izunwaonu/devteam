c4e26a60896e2c1a02c6f05b6843a0d237ad3bf1
swaat <swaatiza@hotmail.co.uk>
2024-03-16 04:09:48 +0100

    updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,6 +14,7 @@ import HeroTwo from '../HeroTwo';
 import Testimonial from '../Testimonial';
 
 import Faqu from '../Faqu';
+import Team from '../team/Team';
 
 const Home = () => {
     useEffect(()=>{
@@ -38,6 +39,7 @@ const Home = () => {
     <OverviewCounter />
     <BannerDetails />
     <SimpleBanner />
+    <Team/>
     <Faqu/>
     <Testimonial/>
     {/* <Blogs /> */}
